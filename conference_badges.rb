43e3e7b74e876dd02cabf0de82454b77df1a89e4
Joseph Guillory <jagmanx99@yahoo.com>
2022-07-10 14:08:16 -0700

    Completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker person
+    "Hello, my name is #{person}."
+end
+
+def batch_badge_creator array
+    array.map do |person| 
+        "Hello, my name is #{person}."
+    end
+end
+
+def assign_rooms array
+    ret_array = []
+    array.each_with_index do |person, idx|
+       ret_array << "Hello, #{person}! You'll be assigned to room #{idx + 1}!"
+    end
+    ret_array
+end
+
+def printer array
+    attendees = batch_badge_creator array
+    rooms = assign_rooms array
+    attendees.map do |line|
+        puts line
+    end
+    rooms.map do |item|
+        puts item
+    end
+end
